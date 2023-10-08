c94005b51dbe3b787615d0cf9233233cf53eb92d
tianchrist <tianchrist@gmail.com>
2023-10-08 20:09:12 +0200

    Zweite Fehlmessung korrigiert

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
 let sek = 0
 let enable = false
 let running = false
-let result = false
-
 let zeitA_ms = 0
 let zeit_ms = 0
-let h_m=0
-
+let result = false
+let h_m = 0
 function Countdown () {
     sek = 3
     for (let index = 0; index < 3; index++) {
-        music.playTone(Note.C, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
         basic.showNumber(sek)
         basic.pause(1000)
         sek += -1
     }
     basic.clearScreen()
-    music.playTone(Note.G, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
 }
 basic.forever(function () {
     while (true) {
@@ -25,24 +23,23 @@ basic.forever(function () {
             enable = true
         }
         if (enable) {
-            if (!running && input.acceleration(Dimension.Strength) < 500) {
+            if (!(running) && input.acceleration(Dimension.Strength) < 500) {
                 zeitA_ms = input.runningTime()
                 if (!(running)) {
                     running = true
                 }
             }
-
             if (running && input.acceleration(Dimension.Strength) > 1000) {
                 zeit_ms = input.runningTime() - zeitA_ms
-                running=false
-                result=true
-            }
-            if (result) {
-                //basic.showNumber(zeit_ms)
-                h_m=9.81/2* (zeit_ms/1000.0)**2
-                basic.showNumber(h_m)    
+                running = false
+                result = true
+                enable=false
             }
         }
-
+        if (result) {
+            // basic.showNumber(zeit_ms)
+            h_m = 9.81 / 2 * (zeit_ms / 1000) ** 2
+            basic.showNumber(h_m)
+        }
     }
 })
